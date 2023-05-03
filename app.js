00c45d0e30191af6926bb04d9d7440e7bea2b081
Baigala <83354086+Baigala@users.noreply.github.com>
2023-05-03 19:31:18 +0800

    5 sariin 3

--- app.js
+++ app.js
@@ -37,12 +37,21 @@
 Өөрийн төрсөн оноо хувьсагчинд хадгалаад одоо байгаа оноос хасч насаа гаргаж ирдэг програм бич.
 */
 var miniiTorsonOn = 1998;
-var currentYear = 2023;
+var currentYear = 2024;
 var nas = currentYear - miniiTorsonOn;
 console.log(nas);
+
+console.log(currentYear - 10);
+console.log(currentYear * 2);
+console.log(currentYear / 2);
 
 /*Дасгал 2
 Гараас радиус авж тухайн радиустай тойргийн урт, талбайг тус тусад нь ол..
 urt = PiToo * 2 * r;
 talbai = PiToo * r * r;
 */
+var radius = +prompt("radiusaa oruulna uu?");
+urt = Math.PI * 2 * radius;
+talbai = Math.PI * radius * radius;
+console.log(urt);
+console.log(talbai);
