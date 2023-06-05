98069b761cae9bd23ba4ce1a2e267261c399818d
Baigala <83354086+Baigala@users.noreply.github.com>
2023-06-05 20:50:10 +0800

    +

---
+++ Module/app.js
@@ -0,0 +1,7 @@
+// named import
+import { num as number, ner } from "./bla.js";
+// default import
+import f from "./bla.js";
+const div = document.getElementById("div");
+div.innerText = f;
+console.log(number);
---
+++ Module/bla.js
@@ -0,0 +1,6 @@
+// named export
+export let num = 55;
+export let ner = "Jess";
+// default export 1 file dotor gants udaa default aar export hj boldog.
+const myColor = "green";
+export default myColor;
